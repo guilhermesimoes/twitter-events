44b6e7c162acb917a2e07073f0b7646184dc0da8
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-11-07 12:22:35 +0000

    Toying around with the Twitter gem.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,19 @@ class TweetsController < ApplicationController
 
   def index
     response.headers["Content-Type"] = "text/event-stream"
-    TWITTER.sample do |tweet|
-      response.stream.write("#{tweet.text}\n")
+
+    # topics = %w(coffee tea)
+    # portugal_bounding_box_coordinates
+    coordinates = %w(-9.580936 36.949892 -6.340828 41.983994)
+
+    conditions = {
+      :locations => coordinates.join(","),
+      :language => "pt"
+      # :track => topics.join(",")
+    }
+
+    TWITTER.filter(conditions) do |tweet|
+      response.stream.write("#{tweet.user.screen_name} says:\n #{tweet.text}\n\n")
       sleep 1
     end
     response.stream.close
