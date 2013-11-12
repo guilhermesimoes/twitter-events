eae6df2b051cbfbd01cabc9c1b100b35abba1c1a
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-11-12 13:18:09 +0000

    Rewrite exception handling.
    Got rid of the begin block since method definitions are implicitly also
    exception blocks.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,15 +10,13 @@ class TweetsController < ApplicationController
     sse = SSE.new(response.stream)
     response.headers["Content-Type"] = "text/event-stream"
 
-    begin
-      twitter_client.filter do |tweet|
-        serialized_tweet = TweetSerializer.new(TweetCreator.create(tweet))
-        sse.write(serialized_tweet)
-      end
-    rescue IOError
-      # When the client disconnects, we'll get an IOError on write
-    ensure
-      sse.close
+    twitter_client.filter do |tweet|
+      serialized_tweet = TweetSerializer.new(TweetCreator.create(tweet))
+      sse.write(serialized_tweet)
     end
+  rescue IOError
+    # When the client disconnects, we'll get an IOError on write
+  ensure
+    sse.close
   end
 end
