0670092f86585f62b47a4760e9a9a19884a1b4cf
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-12-01 23:31:48 +0000

    Add scopes to find tweets by all or any tags and named entities.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,11 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
+  scope :any_tags, -> (*tags){ where("tags && ARRAY[?]", tags) }
+  scope :all_tags, -> (*tags){ where("tags @> ARRAY[?]", tags) }
+  scope :any_named_entities, -> (*named_entities){ where("named_entities && ARRAY[?]", named_entities) }
+  scope :all_named_entities, -> (*named_entities){ where("named_entities @> ARRAY[?]", named_entities) }
+
   def url
     "https://twitter.com/#{user.screen_name}/status/#{twitter_id}"
   end
