af3e4e32934a6eff54b20a096898b00d987a00b7
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-12-01 19:13:30 +0000

    Add simple test to Tweet instance method `url`.

---
+++ test/models/tweet_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+describe Tweet do
+  describe "::url" do
+    it "must return the tweet url on Twitter" do
+      user = User.new(:screen_name => "steve")
+      tweet = Tweet.new(:twitter_id => "12345", :user => user)
+      tweet.url.must_equal "https://twitter.com/steve/status/12345"
+    end
+  end
+end
